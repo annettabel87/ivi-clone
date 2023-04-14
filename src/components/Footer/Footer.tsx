25a0905c1e37ba3fdb301b40cdd97802e177b5b7
unknown <annettabel@rambler.ru>
2023-04-14 11:29:47 +0300

    add footerBody

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import styles from './Footer.module.scss';
 import { FooterHead } from './FooterHead';
+import { FooterBody } from './FooterBody';
 
 export const Footer = () => {
   return (
     <div className={styles.footer}>
       <div className={styles.footer__container}>
         <FooterHead />
+        <FooterBody />
       </div>
     </div>
   );
