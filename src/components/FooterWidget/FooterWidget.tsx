dbf2c404ef8199c9d0c14401b81c803561d15760
unknown <annettabel@rambler.ru>
2023-04-15 20:05:50 +0300

    fix link to widget

--- src/components/FooterWidget/FooterWidget.tsx
+++ src/components/FooterWidget/FooterWidget.tsx
@@ -1,14 +1,15 @@
 import React, { FC } from 'react';
 import mute from '../../assets/icon/mute.png';
 import Image from 'next/image';
+import Link from 'next/link';
 import styles from './FooterWidget.module.scss';
 
 const FooterWidget: FC = () => {
   return (
     <div className={styles.widget}>
-      <button className={styles.widget__btn}>
+      <Link href={'https://www.ivi.ru/subscribe'} className={styles.widget__btn}>
         <Image src={mute} alt={'mute'} />
-      </button>
+      </Link>
       <div className={styles.widget__text}>Смотрите фильмы, сериалы и мультфильмы без рекламы</div>
     </div>
   );
