b7df15068f987d73f4266290b1b7128b0ce32007
unknown <annettabel@rambler.ru>
2023-04-18 12:05:42 +0300

    add  ageRating widget

---
+++ src/components/AgeRating/AgeRating.tsx
@@ -0,0 +1,28 @@
+import React, { FC } from 'react';
+import styles from './AgeRating.module.scss';
+
+export interface IAgeRatingProps {
+  ageRating: number;
+  top?: string;
+  bottom?: string;
+  left?: string;
+  right?: string;
+}
+
+const AgeRating: FC<IAgeRatingProps> = ({ ageRating, top, bottom, left, right }) => {
+  return (
+    <span
+      className={styles.age}
+      style={{
+        top,
+        bottom,
+        left,
+        right,
+      }}
+    >
+      +{ageRating}
+    </span>
+  );
+};
+
+export default AgeRating;
