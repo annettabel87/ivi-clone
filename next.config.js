c3b1880e77cbad1cb0ac5218135d3590762c58f6
unknown <annettabel@rambler.ru>
2023-04-19 19:34:11 +0300

    add images pattern to config

--- next.config.js
+++ next.config.js
@@ -1,6 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'st.kp.yandex.net',
+        port: '',
+        pathname: '/images/actor_iphone/**',
+      },
+    ],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
