e69d0e9c31f57b166d34427e466c780e205b71a4
unknown <annettabel@rambler.ru>
2023-04-19 18:51:24 +0300

    add RetingWidget

---
+++ src/components/FilmCard/FilmInfo/RatingWidget/RatingWidget.tsx
@@ -0,0 +1,37 @@
+import Button from '@/components/Button/Button';
+import React, { FC } from 'react';
+import styles from './RatingWidget.module.scss';
+
+export interface IRatingWidgetProps {
+  rating: number;
+}
+
+const RatingWidget: FC<IRatingWidgetProps> = ({ rating }) => {
+  return (
+    <div className={styles.rating}>
+      <div className={styles.rating__column}>
+        <div className={styles.rating__img}>{rating.toFixed(1)}</div>
+        <div className={styles.rating__description}>
+          <p className={styles.rating__text_white}>Рейтинг Иви</p>
+          <p className={styles.rating__text}>Интересный сюжет</p>
+          <p className={styles.rating__text_small}>21 477 оценок</p>
+        </div>
+      </div>
+      <Button
+        border={'1px solid rgba(255,255,255,0.32)'}
+        bgColor={'transparent'}
+        height={'24px'}
+        radius={'6px'}
+        width={'72px'}
+        as={'button'}
+        hoverBorder={`1px solid #fff`}
+      >
+        Оценить
+      </Button>
+    </div>
+  );
+};
+
+RatingWidget.propTypes = {};
+
+export default RatingWidget;
