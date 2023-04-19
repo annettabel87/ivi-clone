c3960f6594f9e2e381f785ad8991ee462781e834
unknown <annettabel@rambler.ru>
2023-04-19 19:24:45 +0300

    add Medallions

---
+++ src/components/FilmCard/FilmInfo/Medallions/Medallions.tsx
@@ -0,0 +1,44 @@
+import React, { FC } from 'react';
+import Link from 'next/link';
+import Image from 'next/image';
+import styles from './Medallions.module.scss';
+import { IPerson } from '@/pages/film/[filmId]';
+
+export interface IMedallionsProps {
+  persons: IPerson[];
+  rating: number;
+}
+
+const Medallions: FC<IMedallionsProps> = ({ persons, rating }) => {
+  return (
+    <div className={styles.medallions}>
+      <div className={styles.medallions_item}>
+        <div className={styles.medallion}>
+          <div className={styles.medallion_content}>{rating.toFixed(1)}</div>
+        </div>
+        <p className={styles.medallion_caption}>Рейтинг Иви</p>
+      </div>
+
+      {persons.map((person) => {
+        return (
+          <Link href={`/person/${person.id}`} className={styles.medallions_item} key={person.id}>
+            <div className={styles.medallion}>
+              <div className={styles.medallion_content}>
+                <Image
+                  src={person.photo}
+                  className={styles.medallion_img}
+                  alt={'фото'}
+                  width={44}
+                  height={44}
+                />
+              </div>
+            </div>
+            <p className={styles.medallion_caption}>{person.name}</p>
+          </Link>
+        );
+      })}
+    </div>
+  );
+};
+
+export default Medallions;
