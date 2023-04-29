5db4d2375a6c0aaac78bc52af5c9738378832868
unknown <annettabel@rambler.ru>
2023-04-29 21:03:16 +0300

    fix FilmContent

--- src/components/FilmCard/FilmContent/FilmContent.tsx
+++ src/components/FilmCard/FilmContent/FilmContent.tsx
@@ -4,47 +4,30 @@ import { useRouter } from 'next/router';
 import Modal from '@/components/Modal/Modal';
 import { IFilm } from '@/pages/film/[filmId]';
 import ModalTrailer from '../FilmTrailer/ModalTrailer/ModalTrailer';
-import SmallFilmCard from './SmallFilmCard/SmallFilmCard';
+import SmallFilmCard from '../../SmallFilmCard/SmallFilmCard';
 import PersonSmallCard from './SmallPersonCard/PersonSmallCard';
 import TrailerSmallCard from './TrailerSmallCard/TrailerSmallCard';
 import WatchAllDevices from './WatchAllDevices/WatchAllDevices';
 import styles from './FilmContent.module.scss';
 import PersonsModal from './PersonsModal/PersonsModal';
 
-const FilmContent: FC<IFilm> = ({
-  id,
-  similarMovies,
-  trailerLink,
-  actors,
-  director,
-  voice_actors,
-  operator,
-  producers,
-  designers,
-  writers,
-  musics,
-  editor,
-  movieName,
-  poster,
-  ageRating,
-  year,
-}) => {
+const FilmContent: FC<IFilm> = (movie) => {
   const [isOpenTrailers, setIsOpenTrailers] = useState<boolean>(false);
   const router = useRouter();
 
   return (
     <div className={styles.filmContent}>
       <section className={styles.filmContent__films}>
-        <h2 className={styles.filmContent__title}>С фильмом «{movieName}» смотрят</h2>
+        <h2 className={styles.filmContent__title}>С фильмом «{movie.movieName}» смотрят</h2>
         <div className={styles.filmContent__slider}>
-          {similarMovies.map((movie) => {
-            return <SmallFilmCard key={movie.id} {...movie} />;
+          {movie.similarMovies.map((movie) => {
+            return <SmallFilmCard key={movie.id} movie={movie} type={'posterInfo'} />;
           })}
         </div>
       </section>
       <section className={styles.filmContent__persons}>
         <Link
-          href={`/film/${id}/person`}
+          href={`/film/${movie.id}/person`}
           scroll={false}
           shallow={true}
           className={`${styles.filmContent__title} ${styles.title_link}`}
@@ -52,11 +35,11 @@ const FilmContent: FC<IFilm> = ({
           Актёры и создатели
         </Link>
         <div className={styles.filmContent__list}>
-          {[director[0], ...actors].splice(0, 9).map((person) => {
+          {[movie.director[0], ...movie.actors].splice(0, 9).map((person) => {
             return <PersonSmallCard key={person.id} person={person} size={'small'} />;
           })}
           <Link
-            href={`/film/${id}/person`}
+            href={`/film/${movie.id}/person`}
             scroll={false}
             shallow={true}
             className={styles.roundButton}
@@ -68,51 +51,39 @@ const FilmContent: FC<IFilm> = ({
       <section className={styles.filmContent__trailers}>
         <h2 className={styles.filmContent__title}>Трейлеры и доп. материалы </h2>
         <div className={styles.filmContent__slider}>
-          {[0, 0, 0].map((trailer) => {
+          {[movie.trailerLink, movie.trailerLink, movie.trailerLink].map((trailer) => {
             return (
               <TrailerSmallCard
-                key={movieName}
-                poster={poster}
-                movieName={movieName}
+                key={movie.movieName}
+                poster={movie.poster}
+                movieName={movie.movieName}
                 onClickHandler={setIsOpenTrailers}
+                trailer={trailer}
               />
             );
           })}
         </div>
       </section>
-      <WatchAllDevices movieName={movieName} />
+      <WatchAllDevices movieName={movie.movieName} />
       <Modal
         open={router.pathname.includes('person') || isOpenTrailers}
         onClose={
           isOpenTrailers
             ? () => setIsOpenTrailers(false)
             : () => {
-                router.push(`/film/${id}`);
+                router.push(`/film/${movie.id}`);
               }
         }
       >
         {isOpenTrailers ? (
           <ModalTrailer
             onClose={() => setIsOpenTrailers(false)}
-            trailer={trailerLink}
-            ageRating={ageRating}
-            name={movieName}
+            trailer={movie.trailerLink}
+            ageRating={movie.ageRating}
+            name={movie.movieName}
           />
         ) : (
-          <PersonsModal
-            onClose={() => router.push(`/film/${id}`)}
-            movieName={movieName}
-            actors={actors}
-            director={director}
-            voice_actors={voice_actors}
-            operator={operator}
-            producers={producers}
-            designers={designers}
-            writers={writers}
-            musics={musics}
-            editor={editor}
-            year={0}
-          />
+          <PersonsModal onClose={() => router.push(`/film/${movie.id}`)} movie={movie} />
         )}
       </Modal>
     </div>
