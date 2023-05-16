c92748f7a243e909f9316b5ea8280e803da92877
Annettabel <annettabel@rambler.ru>
2023-05-16 22:43:48 +0500

    create api endpoints

---
+++ src/shared/constants/api.ts
@@ -0,0 +1,10 @@
+export const API_IVI_APP = '';
+
+export const API_ENDPOINTS = {
+  FILMS: `${API_IVI_APP}/movies`,
+  FILM: `${API_IVI_APP}/movies/`,
+  REGISTRATION: `${API_IVI_APP}/auth/registration`,
+  LOGIN: `${API_IVI_APP}/auth/login`,
+  ME: `${API_IVI_APP}/users/me'`,
+  PERSON: `${API_IVI_APP}/persons/`,
+};
