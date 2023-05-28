3c98ebeb3731bc2e4399621d4d58497e0dd0c957
Annettabel <annettabel@rambler.ru>
2023-05-28 13:29:51 +0500

    remove token if it not valid

--- src/api/axios-api-instance.ts
+++ src/api/axios-api-instance.ts
@@ -19,27 +19,27 @@ axiosApiInstance.interceptors.request.use(
   }
 );
 
-// axiosApiInstance.interceptors.response.use(
-//   (config) => {
-//     return config;
-//   },
-//   async (error) => {
-//     const originalRequest = error.config;
-//     if (error.response.status == 401 && error.config && !error.config._isRetry) {
-//       originalRequest._isRetry = true;
-//       try {
-//         localStorageActions.removeToken();
-//         // const login = localStorageActions.getLoginData();
-//         // const response = await axios.post<ILoginResponseData>(`${API_ENDPOINTS.LOGIN}`, {
-//         //   ...login,
-//         //   withCredentials: true,
-//         // });
-//         // localStorage.setItem('token', response.data.token);
-//         // return axiosApiInstance.request(originalRequest);
-//       } catch (e) {
-//         Promise.reject(e);
-//       }
-//     }
-//     return Promise.reject(error);
-//   }
-// );
+axiosApiInstance.interceptors.response.use(
+  (config) => {
+    return config;
+  },
+  async (error) => {
+    const originalRequest = error.config;
+    if (error.response.status == 401 && error.config && !error.config._isRetry) {
+      originalRequest._isRetry = true;
+      try {
+        localStorageActions.removeToken();
+        // const login = localStorageActions.getLoginData();
+        // const response = await axios.post<ILoginResponseData>(`${API_ENDPOINTS.LOGIN}`, {
+        //   ...login,
+        //   withCredentials: true,
+        // });
+        // localStorage.setItem('token', response.data.token);
+        // return axiosApiInstance.request(originalRequest);
+      } catch (e) {
+        Promise.reject(e);
+      }
+    }
+    return Promise.reject(error);
+  }
+);
